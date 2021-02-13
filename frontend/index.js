c7ac626c3bd7bfc8f6f71fef453d5e7cbefd0cdc
Chayapol-c <chayapol.cha@ku.th>
2021-02-13 15:13:45 +0700

    config fetch data

--- frontend/index.js
+++ frontend/index.js
@@ -5,50 +5,18 @@ const status = document.querySelectorAll(".status")
 const timeDetails = document.querySelectorAll(".time")
 const showGraphBtn = document.querySelector(".btn-graph")
 const graph = document.querySelector("#chartContainer")
-
-
+const time = document.querySelectorAll(".time>p")
 
 const changeStage = (index) =>{
     cards[index].style.cssText = "background: hsl(359, 100%, 65%)"
     timeDetails[index].style.cssText = "visibility: visible;"
 }
-
-const car1 =    {
-    "_id": "60273f32f56e3c00070ec8a9",
-    "nameCarPark": "1",
-    "isAvailable": false,
-    "timeIn" : "2020-01-01 12:00:00"
-}
-
-const car2 = {
-    "_id": "602756891fa18f0ce9edc119",
-    "nameCarPark": "2",
-    "isAvailable": false,
-    "timeIn": "2021-02-13 12:00:21"
-}
-
-const car3 = {
-    "_id": "6027569e1fa18f0ce9edc11a",
-    "nameCarPark": "3",
-    "isAvailable": true,
-    "timeIn": "2021-02-13 11:33:34"
-}
-
-const car4 = {
-    "_id": "60275abc3cb64f65dce1fcd7",
-    "nameCarPark": "4",
-    "isAvailable": true,
-    "timeIn": "2021-02-13 11:51:08"
-}
-
-
-const cars = [car1, car2, car3, car4]
 const costs = []
 
 const updateData = (data) =>{
-    data.forEach(ele => {
-        if(!ele.isAvailable){
-            let index = Number(ele.nameCarPark)
+    data.result.forEach(element => {
+        if(!element.isAvailable){
+            let index = Number(element.nameCarPark)
             changeStage(index-1);
         }
     });
@@ -61,10 +29,9 @@ const getData = () =>{
             'Content-Type' : 'application/json',
             "Access-Control-Allow-Origin": "*",
         }})
-        .then(res => {res.json()
-        console.log(res)})
+        .then(res => res.json())
         .then(data => {
-            console.log(data)
+            window.setInterval(updateData(data), 2000 );
         })
         .catch(err => {
            console.log(err.message)
@@ -104,10 +71,8 @@ window.onload = function () {
 
 const showGraph = () =>{
     graph.classList.toggle("hidden")
-    console.log(graph)
-
 }
 
 showGraphBtn.addEventListener("click", showGraph)
 window.setInterval(getData, 2000 );
-window.setInterval(updateData(cars), 2000 );
+
